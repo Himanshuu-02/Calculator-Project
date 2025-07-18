c3134e7ddcb92c7bce69a4d2fcadf9560104a6cf
Himanshuu-02 <hs282835@gmail.com>
2025-07-18 12:00:25 +0530

    First commit

---
+++ src/Calculator/Button.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "../Calc.css";
+
+function Button() {
+  const [result, setresult] = useState("");
+  const handleClick = (e) => {
+    setresult(result.concat(e.target.value));
+  };
+  const ClearHandle = () => {
+    setresult("");
+  };
+  const Calculate = () => {
+    setresult(eval(result).toString());
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="0" id="answer" value={result} />
+      </div>
+
+      <input type="button" value="9" className="button" onClick={handleClick} />
+      <input type="button" value="8" className="button" onClick={handleClick} />
+      <input type="button" value="7" className="button" onClick={handleClick} />
+      <input type="button" value="6" className="button" onClick={handleClick} />
+      <input type="button" value="5" className="button" onClick={handleClick} />
+      <input type="button" value="4" className="button" onClick={handleClick} />
+      <input type="button" value="3" className="button" onClick={handleClick} />
+      <input type="button" value="2" className="button" onClick={handleClick} />
+      <input type="button" value="1" className="button" onClick={handleClick} />
+      <input type="button" value="0" className="button" onClick={handleClick} />
+      <input type="button" value="+" className="button" onClick={handleClick} />
+      <input type="button" value="-" className="button" onClick={handleClick} />
+      <input type="button" value="*" className="button" onClick={handleClick} />
+      <input type="button" value="/" className="button" onClick={handleClick} />
+      <input type="button" value="%" className="button" onClick={handleClick} />
+      <input type="button" value="." className="button" onClick={handleClick} />
+      <input
+        type="button"
+        value="Clear"
+        className="button button1"
+        onClick={ClearHandle}
+      />
+      <input
+        type="button"
+        value="="
+        className="button button1"
+        onClick={Calculate}
+      />
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/Calculator/Output.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import '../Calc.css'
+
+
+// function Output() {
+// //   return (
+// //     // <div><input type="text" placeholder="0" id='answer' value={value}/></div>
+// //   )
+// }
+
+// export default Output
